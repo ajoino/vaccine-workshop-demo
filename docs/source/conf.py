4afde7f74ff536a3b219a1267ada1742cd9c832e
ajoino <jacob.nilsson@ltu.se>
2021-04-13 10:41:01 +0200

    Initial commit

---
+++ docs/source/conf.py
@@ -0,0 +1,74 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('..'))
+import _version
+import datetime
+
+
+# -- Project information -----------------------------------------------------
+
+project = _version.__lib_name__
+copyright = f'2020-{datetime.date.today().year}, {_version.__author__}'
+author = _version.__author__
+
+# The full version, including alpha/beta/rc tags
+release = _version.__version__
+version = release
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.coverage',
+    'sphinx.ext.napoleon',
+    'sphinx_rtd_theme',
+    'sphinx_autodoc_typehints',
+    'enum_tools.autoenum',
+    'sphinxawesome_theme',
+]
+
+autodoc_typehints = 'description'
+autoclass_content = 'both'
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'sphinxawesome_theme'
+html_logo = '_static/arrowhead-logo.svg'
+html_collapsible_definitions = True
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+html_css_files = ['arrowhead_custom.css']
+
+print(sys.executable)
